386c76b7aa5c45a866c2e62415e9bef07cd21442
midhulakp <115697790+midhulakp@users.noreply.github.com>
2022-10-18 15:03:46 +0530

    Create turtlesquare.py
    python turtle program to print a square

---
+++ Python/turtlesquare.py
@@ -0,0 +1,11 @@
+import turtle  
+   
+ttl = turtle.Turtle()  
+ttl.forward(150)  
+ttl.left(90)   
+ttl.forward(150)  
+ttl.left(90)  
+ttl.forward(150)  
+ttl.left(90)  
+ttl.forward(150)  
+ttl.left(90)  
